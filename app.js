7d40fad274a25c51247015129462682591b86401
Brett Kinnamon <b.kinnamon@icloud.com>
2020-06-20 13:35:11 -0400

    finish up the registrations data type

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const regRoutes = require('./src/routes/registrations.js');
 
 const PORT = 3000;
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
--- src/registrations.js
+++ src/registrations.js
@@ -3,10 +3,19 @@ const errors = require('./errors.js');
 
 const collectionName = process.env.DEBUG ? 'test_registrations' : 'registrations';
 
+function generate(data) {
+  const reg = data;
+  if (reg.notes === undefined || reg.notes == null) reg.notes = '';
+  if (!reg.pickup || reg.pickup === null) reg.pickup = { driverName: '', driverId: '' };
+  if (!reg.return || reg.return === null) reg.return = { driverName: '', driverId: '' };
+  return reg;
+}
+
 function isValid(data) {
   if (!data.familyId) return false;
   if (!Array.isArray(data.children) || data.children.length === 0) return false;
   if (!Array.isArray(data.adults) || data.adults.length === 0) return false;
+  if (data.pickup === undefined || data.return === undefined) return false;
 
   return true;
 }
@@ -19,31 +28,37 @@ function docToReg(doc) {
 }
 
 async function create(data) {
-  if (!isValid(data)) return [null, errors.badRequest];
+  const reg = generate(data);
+  if (!isValid(reg)) return [null, errors.badRequest];
   const docRef = await db.collection(collectionName).add(data);
   const doc = await docRef.get();
+
   return [docToReg(doc), null];
 }
 
 async function getAll() {
   const collection = await db.collection(collectionName).get();
   const regs = collection.docs.map(docToReg);
+
   return [regs, null];
 }
 
 async function get(id) {
   const doc = await db.collection(collectionName).doc(id).get();
   if (!doc.exists) return [null, errors.notFound];
+
   return [docToReg(doc), null];
 }
 
 async function update(id, data) {
-  if (!isValid(data) || !id) return [null, errors.badRequest];
+  const reg = generate(data);
+  if (!isValid(reg) || !id) return [null, errors.badRequest];
   try {
     await db.collection(collectionName).doc(id).update(data);
   } catch (err) {
     return [null, errors.notFound];
   }
+
   return [{ ...data, id }, null];
 }
 
